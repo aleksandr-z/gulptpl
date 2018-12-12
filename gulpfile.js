34a3bb839cf20927001eea8b2a7870300648ccaa
Aleksandr <aleksandrzvyagin@yandex.ru>
2018-12-12 22:18:53 +0300

    create template gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function(){
+	return gulp.src('app/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('public'))
+});
+
+gulp.task('del', function(){
+	return del('public');
+})
+
+gulp.task('assets', function(){
+	return gulp.src('app/assets/**')
+		.pipe(gulp.dest('public'));
+})
+
+gulp.task('build', gulp.series('del', gulp.parallel('sass','assets')));
